dc70fa3ab74a34defa2b2ddec08688c553c2f7f9
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2019-08-16 19:15:16 +0100

    stage scene and temp script
    comprised of 6 string models, a camera and a script that randomly places a note on top of the respective string while also applying the appropriate material

---
+++ src/stage/Strings.gd
@@ -0,0 +1,21 @@
+extends Spatial
+
+onready var note = preload("res://src/note/Note.tscn")
+
+var notes = []
+const timedelta = 2.0
+var time = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time += delta
+	if time >= timedelta:
+		time -= timedelta
+		var note_string = randi() % 6 + 1
+		var noteObj = note.instance()
+		noteObj.init(note_string)
+		noteObj.translate( Vector3(0.0, note_string * 0.5, 0.1))
+		$Notes.add_child(noteObj)
