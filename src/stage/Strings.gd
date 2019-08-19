7fe6d38c12d88234a14d637b79a4af842de170a4
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2019-08-19 06:32:32 +0100

    notes spawn more frequently and in less fret pos

--- src/stage/Strings.gd
+++ src/stage/Strings.gd
@@ -3,7 +3,7 @@ extends Spatial
 onready var note = preload("res://src/note/Note.tscn")
 
 var notes = []
-const timedelta = 2.0
+const timedelta = 0.5
 var time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,8 +15,8 @@ func _process(delta):
 	if time >= timedelta:
 		time -= timedelta
 		var note_string = randi() % 6 + 1
-		var note_fret = -(randi() % 8) + 8
+		var note_fret = -(randi() % 3) + 3
 		var noteObj = note.instance()
 		noteObj.init(note_string)
-		noteObj.translate( Vector3(-9.0 + note_fret * 2.0, note_string * 0.5, -10.0))
+		noteObj.translate( Vector3(-3.0 + note_fret * 2.0, note_string * 0.5, -10.0))
 		$Notes.add_child(noteObj)
