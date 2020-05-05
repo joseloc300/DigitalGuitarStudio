47250a6da4111621e68ec0c477697879877944b9
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2020-05-05 20:08:00 +0100

    fixed script errors

--- src/objects/note_3d/note_3d.gd
+++ src/objects/note_3d/note_3d.gd
@@ -1,11 +1,11 @@
 extends Spatial
 
-const red_mat = preload("res://src/objects/note/red_note_mat.tres")
-const yellow_mat = preload("res://src/objects/note/yellow_note_mat.tres")
-const blue_mat = preload("res://src/objects/note/blue_note_mat.tres")
-const orange_mat = preload("res://src/objects/note/orange_note_mat.tres")
-const green_mat = preload("res://src/objects/note/green_note_mat.tres")
-const purple_mat = preload("res://src/objects/note/purple_note_mat.tres")
+const red_mat = preload("res://src/objects/note_3d/red_note_mat.tres")
+const yellow_mat = preload("res://src/objects/note_3d/yellow_note_mat.tres")
+const blue_mat = preload("res://src/objects/note_3d/blue_note_mat.tres")
+const orange_mat = preload("res://src/objects/note_3d/orange_note_mat.tres")
+const green_mat = preload("res://src/objects/note_3d/green_note_mat.tres")
+const purple_mat = preload("res://src/objects/note_3d/purple_note_mat.tres")
 
 const mats = [red_mat, yellow_mat, blue_mat, orange_mat, green_mat, purple_mat]
 const inverted_index_correction = 5
@@ -50,21 +50,21 @@ func _process(delta):
 	if translation.z >= 0:
 #		$CSGBox.visible = false
 		if !frozen && tailLength != 0:
-			$CSGBox.translation.z = 0
-			$CSGBox.set_ignore_transform_notification(true)
+			$note.translation.z = 0
+			$note.set_ignore_transform_notification(true)
 		updateTail(movement)
 	if translation.z - tailLength >= 0:
 		queue_free()
 
 func set_color(string):
-	$CSGBox.material = mats[string]
-	$CSGTail.material = mats[string]
+	$note.material = mats[string]
+	$tail.material = mats[string]
 
 func initTailLength():
 	tailLength = sustain * scrollSpeed
-	$CSGTail.depth = tailLength
-	$CSGTail.translate(Vector3(0, 0 , -tailLength/2.0))
+	$tail.depth = tailLength
+	$tail.translate(Vector3(0, 0 , -tailLength/2.0))
 
 func updateTail(movement):
-	$CSGTail.depth -= movement.z
-	$CSGTail.translate(-movement/2)
+	$tail.depth -= movement.z
+	$tail.translate(-movement/2)
