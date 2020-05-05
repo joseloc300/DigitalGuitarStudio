0e03a76d2fbf593cdce25fa9f8505fdd4d4e7209
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2020-05-05 14:04:02 +0100

    fretlanes can be activated/deactivated, new material

---
+++ src/objects/fretlane/fretlane.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+
+var default_mat = preload("res://src/objects/fretlane/fretlane_mat.tres")
+var active_mat = preload("res://src/objects/fretlane/fretlane_active_mat.tres")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func activate_lane():
+	$CSGBox.material = active_mat
+
+func deactivate_lane():
+	$CSGBox.material = default_mat
