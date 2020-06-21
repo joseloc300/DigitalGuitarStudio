f549b5e65c153b7ffb38ea16931631a378d0224f
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2020-06-21 07:48:48 +0100

    forgotten stuff to remove from last commit
    removed ui related to real-time fx, feature dropped (that was forgotten last commit)

--- src/menus/main_menu/main_menu.gd
+++ src/menus/main_menu/main_menu.gd
@@ -14,28 +14,5 @@ func _on_btn_play_pressed():
 	get_tree().change_scene("res://src/menus/song_list/song_list.tscn")
 
 
-func _on_btn_scan_songs_pressed():
-	var files = []
-	var dir = Directory.new()
-	dir.open("./songs/")
-	dir.list_dir_begin()
-	
-	while true:
-		var file = dir.get_next()
-		if file == "":
-			break
-		elif dir.current_is_dir() && file != "." && file != "..":
-			files.append(file)
-	
-	dir.list_dir_end()
-	
-	$Label.text = "Songs scanned: " + str(files.size())
-	
-	$OptionButton.clear()
-	
-	for i in range(files.size()):
-		$OptionButton.add_item(files[i])
-
-
 func _on_btn_exit_pressed():
 	get_tree().quit(0)
